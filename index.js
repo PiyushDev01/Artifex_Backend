78ae36b260f87bedbcb7f5dd56f0883f58567c4b
PiyushDev01 <piyushvishwakarma6707@gmail.com>
2024-10-20 22:16:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+app.get('/' , (req,res)=>{
+    res.send('Artifex Server is running on port 3000');
+})
+
+app.listen (port, ()=>{
+    console.log("Server is running on port 3000");
+    
+})
+
+
