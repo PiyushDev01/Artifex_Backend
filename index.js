fc56a83eeaec0b21c13c9980db27a20c7f683434
Piyush <piyushvishwakarma6707@gmail.com>
2024-11-14 19:16:53 +0530

    razorpay added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const app = express();
 const axios = require('axios');
 const nodemailer = require('nodemailer');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const razorpay = require('razorpay');
+const crypto = require('crypto');   
+
+
 dotenv.config();
+const app = express();
 
 const port = process.env.PORT || 3000;
 const delhivery_api = process.env.DELHIVERY_API;
@@ -17,9 +21,7 @@ app.use(express.json());
 app.get('/' , (req,res)=>{
     res.send(`Artifex Server is running on port ${port}`);
 })
-app.get('/home' , (req,res)=>{
-    res.send(`Welcome to Artifex Server`);
-})
+
 
 app.get('/api/pin-codes', async (req, res) => {
   try {
@@ -38,6 +40,7 @@ app.get('/api/pin-codes', async (req, res) => {
 });
 
 
+  //this is for mailing functionality
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -67,6 +70,45 @@ const transporter = nodemailer.createTransport({
   });
 
 });
+
+app.post("/order", async(req, res)=>{
+
+  try{
+       const razorpayInstance = new razorpay({
+      key_id: process.env.RAZORPAY_KEY_ID,
+      key_secret: process.env.RAZORPAY_KEY_SECRET
+  });
+
+  const options = req.body;
+  const order = await razorpayInstance.orders.create(options);
+  if(!order){
+      return res.status(500).json({error: "Some error occured"});
+  }
+  res.json(order);
+  }catch(err){
+      console.log(err);
+      res.status(500).json({error: "Some error occured"});
+  }
+ 
+})
+
+app.post("/capture/validate", async(req, res)=>{
+  const {razorpay_payment_id,razorpay_order_id,razorpay_signature} = req.body;
+  const sha = crypto.createHmac('sha256', process.env.RAZORPAY_KEY_SECRET);
+  //order_id + "|" + razorpay_payment_id, secret
+  sha.update(`${razorpay_order_id}|${razorpay_payment_id}`);
+     const digest = sha.digest('hex');
+     if(digest !== razorpay_signature){
+         return res.status(400).json({error: "Invalid signature"});
+     }
+     res.json({
+         msg: "success",
+         order_id: razorpay_order_id,
+         paymentId: razorpay_payment_id 
+     });
+});
+
+
 
 
 
